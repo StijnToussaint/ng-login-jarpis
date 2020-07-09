8b3eb1204c7dafd009424e523666583db2a2d4dc
Stijn <stijn@mbp-van-stijn.home>
2020-07-10 00:12:46 +0200

    New user added

--- src/app/services/new-user.service.ts
+++ src/app/services/new-user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class NewUserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  newUser(newUser){
+
+    return this.http.post("https://testapi.jarpiscloud.nl/api/v1/users", newUser);
+  }
 }
