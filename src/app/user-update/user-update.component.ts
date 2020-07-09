1874498dc02a96a0fe030adc90b1d15714b09cd7
Stijn <stijn@mbp-van-stijn.home>
2020-07-09 23:11:25 +0200

    Form created

--- src/app/user-update/user-update.component.ts
+++ src/app/user-update/user-update.component.ts
@@ -1,6 +1,7 @@
 import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-update',
@@ -10,19 +11,36 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class UserUpdateComponent implements OnInit {
   user;
   id;
-  constructor(private route: ActivatedRoute, private usersService: UsersService, private router: Router) { }
+  firstName = new FormControl("");
+  lastName = new FormControl("");
+  email = new FormControl("");
+  password = new FormControl("");
+  avatar = new FormControl("");
+
+
+  constructor(private route: ActivatedRoute, private usersService: UsersService, private router: Router) { 
+  }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params["id"] - 1;
-    this.usersService.getUsers(this.id,1).subscribe(
+    this.id = this.route.snapshot.params["id"];
+    this.usersService.getUsers(this.id - 1,1).subscribe(
       result => {
         this.user = result["data"][0];
+        this.firstName.setValue(this.user["firstName"]);
+        this.lastName.setValue(this.user["lastName"]);
+        this.email.setValue(this.user["email"]);
+        this.password.setValue(this.user["password"]);
+        this.avatar.setValue(this.user["avatar"]);
       }
     );
   }
 
   back(){
-    this.router.navigateByUrl("private-home/"+this.route.snapshot.params["id"]);
+    this.router.navigateByUrl("private-home/"+this.id);
+  }
+
+  update(){
+    //Pass info to service
   }
 
 }
