de8acca5be3e69cb35795ad8029fbfe31c8aed5c
Stijn <stijn@mbp-van-stijn.home>
2020-07-10 10:41:27 +0200

    Basic pagination added

--- src/app/private-home/private-home.component.ts
+++ src/app/private-home/private-home.component.ts
@@ -11,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class PrivateHomeComponent implements OnInit {
   authService;
   users;
+  startIndex = 0;
+  endIndex = 10;
   constructor(private _authService: AuthService, private usersService: UsersService, private router: Router) { 
     this.authService = _authService;
   }
@@ -25,6 +27,16 @@ export class PrivateHomeComponent implements OnInit {
 
   newUser(){
     this.router.navigateByUrl("private-home/new-user");
+  }
+
+  getArrayFromNumber(length){
+    return new Array(Math.ceil(length/10));
+  }
+
+  updateIndex(pageIndex){
+    this.startIndex = pageIndex * 10;
+    this.endIndex = this.startIndex + 10;
+
   }
 
 }
