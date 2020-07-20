4dd392e1a03a1a9b3e6c0c58e32d425c6e93dc5d
StijnToussaint <stijn@saintonline.nl>
2020-07-20 16:38:38 +0200

    Update user validation

--- src/app/user-update/user-update.component.ts
+++ src/app/user-update/user-update.component.ts
@@ -2,7 +2,7 @@ import { UpdateUserService } from './../services/update-user.service';
 import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-update',
@@ -12,11 +12,18 @@ import { FormControl } from '@angular/forms';
 export class UserUpdateComponent implements OnInit {
   user;
   id;
-  firstName = new FormControl("");
-  lastName = new FormControl("");
-  email = new FormControl("");
-  password = new FormControl("");
-  avatar = new FormControl("");
+  //All regex used in this formgroup are plucked from the internet:
+  //password : https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+  //avatar link : https://mathiasbynens.be/demo/url-regex
+  updateUserForm = new FormGroup({
+    firstName : new FormControl("", [Validators.required, Validators.minLength(2)]),
+    lastName : new FormControl("", [Validators.required, Validators.minLength(2)]),
+    email : new FormControl("", [Validators.required, Validators.email]),
+    password : new FormControl("", [Validators.required, Validators.minLength(4), 
+      Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{4,}$")]),
+    avatar : new FormControl("", [Validators.required, Validators.minLength(2),
+      Validators.pattern("^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x00a1-\xffff0-9]+-?)*[a-z\x00a1-\xffff0-9]+)(?:\.(?:[a-z\x00a1-\xffff0-9]+-?)*[a-z\x00a1-\xffff0-9]+)*(?:\.(?:[a-z\x00a1-\xffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$")]),
+  })
   invalidUpdate;
 
   constructor(private route: ActivatedRoute, private usersService: UsersService, 
@@ -28,11 +35,13 @@ export class UserUpdateComponent implements OnInit {
     this.usersService.getUsersById(this.id).subscribe(
       result => {
         this.user = result;
-        this.firstName.setValue(this.user["firstName"]);
-        this.lastName.setValue(this.user["lastName"]);
-        this.email.setValue(this.user["email"]);
-        this.password.setValue(this.user["password"]);
-        this.avatar.setValue(this.user["avatar"]);
+        this.updateUserForm.setValue({
+          firstName:this.user["firstName"],
+          lastName:this.user["lastName"],
+          email:this.user["email"],
+          password:this.user["password"],
+          avatar:this.user["avatar"]
+        });
       }
     );
   }
@@ -43,11 +52,11 @@ export class UserUpdateComponent implements OnInit {
 
   update(){
     let newUser = {
-      "avatar":this.avatar.value,
-      "email":this.email.value,
-      "firstName":this.firstName.value,
-      "lastName":this.lastName.value,
-      "password":this.password.value
+      "avatar":this.updateUserForm.get("avatar").value,
+      "email":this.updateUserForm.get("email").value,
+      "firstName":this.updateUserForm.get("firstName").value,
+      "lastName":this.updateUserForm.get("lastName").value,
+      "password":this.updateUserForm.get("password").value
     }
     this.updateUserService.updateUser(this.id,newUser).subscribe(
       result => {
@@ -58,6 +67,22 @@ export class UserUpdateComponent implements OnInit {
         }
       }
     );
+  }
+
+  get avatar(){
+    return this.updateUserForm.get('avatar');
+  }
+  get email(){
+    return this.updateUserForm.get('email');
+  }
+  get firstName(){
+    return this.updateUserForm.get('firstName');
+  }
+  get lastName(){
+    return this.updateUserForm.get('lastName');
+  }
+  get password(){
+    return this.updateUserForm.get('password');
   }
 
 }
